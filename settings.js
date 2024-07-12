5eaf5b6af58e4528e3b0aac3e7b04d794569d037
Joker165 <145442092+Joker165@users.noreply.github.com>
2024-07-12 03:23:14 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,33 +13,33 @@ const chalk = require('chalk')
 //owmner v card
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.location = "Nigeria, Lagos, africa" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V13' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
-global.websitex = "https://youtu.be/N3kyujoG3vk"
-global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
-global.themeemoji = '🪀'
+global.botname = 'ᕼᗴᖇ ᗞᗩᏆᏞᎩ 🥹ᗷᔑᗴᔑᔑᏆᝪᑎ' //ur bot name
+global.ownernumber = '2349124580380' //ur owner number
+global.ownername = 'ᕼᗴᖇ ᗞᗩᏆᏞᎩ 🥹ᗷᔑᗴᔑᔑᏆᝪᑎ' //ur owner name
+global.websitex = "https://chat.whatsapp.com/BTTuDZX787lLk6JmIEE71a"
+global.wagc = "https://chat.whatsapp.com/BTTuDZX787lLk6JmIEE71a"
+global.themeemoji = '🥹'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD13' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "ᕼᗴᖇ ᗞᗩᏆᏞᎩ 🥹ᗷᔑᗴᔑᔑᏆᝪᑎ"
+global.creator = "2349124580380@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["2349124580380"] // Premium User
 global.hituet = 0
 
 //bot sett
-global.typemenu = 'v12' // menu type 'v1' => 'v12'
+global.typemenu = 'v8' // menu type 'v1' => 'v12'
 global.typereply = 'v4' // reply type 'v1' => 'v4'
-global.autoblocknumber = '92' //set autoblock country code
-global.antiforeignnumber = '91' //set anti foreign number country code
-global.welcome = false //welcome/left in groups
+global.autoblocknumber = '212' //set autoblock country code
+global.antiforeignnumber = '212' //set anti foreign number country code
+global.welcome = true //welcome/left in groups
 global.anticall = false //bot blocks user when called
-global.autoswview = false //auto status/story view
-global.adminevent = false //show promote/demote message
+global.autoswview = true //auto status/story view
+global.adminevent = true //show promote/demote message
 global.groupevent = false //show update messages in group chat
 //msg
 global.mess = {
